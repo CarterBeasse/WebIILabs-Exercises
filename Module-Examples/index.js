2901a17eb1b9453438b61c6aad06bcb6a67159ad
unknown <196205081+CarterBeasse@users.noreply.github.com>
2026-03-16 12:13:40 -0400

    More exercises

---
+++ Module-Examples/index.js
@@ -0,0 +1,7 @@
+import {add} from "./math"
+
+console.log(add(2,3));
+
+const root = document.getElementById("root");
+
+root.textContent = `2 + 3`
---
+++ Module-Examples/math.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * @param {*} a 
+ * @param {*} b 
+ * @returns 
+ */
+export function add(a, b) {
+    return a + b;
+}
